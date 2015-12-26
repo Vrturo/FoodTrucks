74b3bff6b2a5390ba44362161de6935ec3b197d7
Arturo Perez <VrturoPerez@gmail.com>
2015-12-25 23:23:18 -0800

    include jQuery

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,34 +1,35 @@
-var location = document.getElementById("location");
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        location.innerHTML = "Geolocation is not supported by this browser.";
-    }
-}
-var myMap = {};
-function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude +
-    "<br>Longitude: " + position.coords.longitude;
-    initMap(position.coords.latitude, position.coords.longitude);
-}
-var map;
-  function initMap(latt, lngg) {
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: latt, lng: lngg},
-      zoom: 15
-    });
-    var marker = new google.maps.Marker({
-      position: map.getCenter(),
-      icon: {
-        path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
-        scale: 5
-      },
-      draggable: false,
-      map: map
-    });
-  }
+// var location = document.getElementById("location");
+// function getLocation() {
+//     if (navigator.geolocation) {
+//         navigator.geolocation.getCurrentPosition(showPosition);
+//     } else {
+//         location.innerHTML = "Geolocation is not supported by this browser.";
+//     }
+// }
+// var myMap = {};
+// function showPosition(position) {
+//     x.innerHTML = "Latitude: " + position.coords.latitude +
+//     "<br>Longitude: " + position.coords.longitude;
+//     initMap(position.coords.latitude, position.coords.longitude);
+// }
+// var map;
+//   function initMap(latt, lngg) {
+//     map = new google.maps.Map(document.getElementById('map'), {
+//       center: {lat: latt, lng: lngg},
+//       zoom: 15
+//     });
+//     var marker = new google.maps.Marker({
+//       position: map.getCenter(),
+//       icon: {
+//         path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
+//         scale: 5
+//       },
+//       draggable: false,
+//       map: map
+//     });
+//   }
 
 $( document ).ready(function() {
-  initMap();
+  // initMap();
+  console.log("hello")
 });

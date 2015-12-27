bf53aebd77df66c9608214f18ceecd40958ddd40
Arturo Perez <VrturoPerez@gmail.com>
2015-12-26 17:03:56 -0800

    show current location coordinates

--- public/javascripts/location.js
+++ public/javascripts/location.js
@@ -1,18 +1,15 @@
-var location = document.getElementById("location");
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        location.innerHTML = "Geolocation is not supported by this browser.";
-    }
-};
-function showPosition(position) {
-    location.innerHTML = "Latitude: " + position.coords.latitude +
-    "<br>Longitude: " + position.coords.longitude;
-};
-// function start() {
-//   // initialize();
-//   getLocation();
-// }
-// window.onload = start;
-// window.onload = getLocation();
+document.addEventListener('DOMContentLoaded', function () {
+  var location = document.getElementById("location");
+  function showPosition(position) {
+      location.innerHTML = "Latitude: " + position.coords.latitude +
+      "<br>Longitude: " + position.coords.longitude;
+  };
+  function getLocation() {
+      if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(showPosition);
+      } else {
+          location.innerHTML = "Geolocation is not supported by this browser.";
+      }
+  };
+  getLocation();
+});

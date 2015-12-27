a385ffb8753dfe3daed48386db24d309a9e9a735
Arturo Perez <VrturoPerez@gmail.com>
2015-12-26 16:00:49 -0800

    add map.js with marker listeners

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,24 +1,34 @@
-
-//  function initMap() {
-//   GoogleMapsLoader.load(function(google) {
-//     new google.maps.Map(document.getElementById('map'), {
-//     center: {lat: -34.397, lng: 150.644},
-//     zoom: 8
-//     });
-//   });
-// }
-
-
-
-
+var berlin = new google.maps.LatLng(52.520816, 13.410186);
+var neighborhoods = [
+    new google.maps.LatLng(52.511467, 13.447179),
+    new google.maps.LatLng(52.549061, 13.422975),
+    new google.maps.LatLng(52.497622, 13.396110),
+    new google.maps.LatLng(52.517683, 13.394393)
+  ];
+var markers = [];
+var iterator = 0;
 var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
-  });
+function initialize() {
+    var mapOptions = {
+    zoom: 12,
+    center: berlin
+  };
+map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 }
-
-$( document ).ready(function() {
-  initMap();
-});
+function drop() {
+  for (var i = 0; i < neighborhoods.length; i++) {
+    setTimeout(function() {
+    addMarker();
+    }, i * 200);
+  }
+}
+function addMarker() {
+  markers.push(new google.maps.Marker({
+    position: neighborhoods[iterator],
+    map: map,
+    draggable: false,
+    animation: google.maps.Animation.DROP
+  }));
+  iterator++;
+}
+google.maps.event.addDomListener(window, 'load', initialize);

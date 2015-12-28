1a939fcbb54fddad0c966e84ada9a6cdbc37fd5f
Arturo Perez <VrturoPerez@gmail.com>
2015-12-27 17:46:53 -0800

    get Sf food data to appear on console

--- public/javascripts/datasf.js
+++ public/javascripts/datasf.js
@@ -1,1 +1,9 @@
-datasf.js
+var request = new XMLHttpRequest();
+request.open("GET", "https://data.sfgov.org/resource/6a9r-agq8.json", true);
+request.send();
+request.onreadystatechange = function () {
+  if (request.readyState != 4 || request.status != 200) return;
+  alert("Success: " + request.responseText);
+};
+
+console.log(request)

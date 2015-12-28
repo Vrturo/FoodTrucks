732ab18ad01ed24ada24cd9c125fb38b21071fa8
Arturo Perez <VrturoPerez@gmail.com>
2015-12-27 18:00:18 -0800

    get only sf data to appear on console

--- public/javascripts/datasf.js
+++ public/javascripts/datasf.js
@@ -1,9 +1,10 @@
-var request = new XMLHttpRequest();
-request.open("GET", "https://data.sfgov.org/resource/6a9r-agq8.json", true);
-request.send();
-request.onreadystatechange = function () {
-  if (request.readyState != 4 || request.status != 200) return;
-  alert("Success: " + request.responseText);
-};
-
-console.log(request)
+document.addEventListener('DOMContentLoaded', function () {
+  var request = new XMLHttpRequest();
+  request.open("GET", "https://data.sfgov.org/resource/6a9r-agq8.json", true);
+  request.send();
+  request.onreadystatechange = function () {
+    if (request.readyState != 4 || request.status != 200) return;
+    console.log(request.responseText);
+  };
+    document.getElementById("data").innerHTML = request.responseText;
+});

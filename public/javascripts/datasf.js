097a5993dfe2d1a98a603f268b75b9f39e59b864
Arturo Perez <VrturoPerez@gmail.com>
2015-12-28 21:38:09 -0800

    switch geocode to map.js file

--- public/javascripts/datasf.js
+++ public/javascripts/datasf.js
@@ -1,3 +1,5 @@
+
+
 document.addEventListener('DOMContentLoaded', function () {
   var request = new XMLHttpRequest();
   request.open("GET", "https://data.sfgov.org/resource/6a9r-agq8.json", true);
@@ -20,5 +22,32 @@ document.addEventListener('DOMContentLoaded', function () {
             "<br><strong>Food</strong>: " + objArr[i].fooditems + "<br>";
           ol.appendChild(li);
         }; // for
+
+        // Create a listener on a button click to geocode the FoodTruck's addresses
+        // var geocoder = new google.maps.Geocoder();
+        // var showTrucks = document.getElementById("showTrucks");
+        // var myMap = google.maps.Map;
+        // showTrucks.addEventListener('click', function(e){
+        //   e.preventDefault();
+        //   console.log(myMap);
+        //   geocodeAddress(geocoder, myMap, objArr);
+        // });
+        // function geocodeAddress(geocoder, resultsMap) {
+        //   for (i = 0; i < objArr.length; i++) {
+        //     var address = JSON.stringify(objArr[i].address)
+        //     geocoder.geocode({'address': address}, function(results, status) {
+        //       if (status === google.maps.GeocoderStatus.OK) {
+        //         resultsMap.setCenter(results[0].geometry.location);
+        //         var marker = new google.maps.Marker({
+        //           map: resultsMap,
+        //           position: address[0].geometry.location,
+        //           icon: "http://howtostartafoodtruckbusinessinindia.com/wp-content/uploads/foodtruck-icon-web.png",
+        //           draggable: false,
+        //           map: map,
+        //         });
+        //       };
+        //     });
+        //   }; // for
+        // }; // geocode
     }; // if
 }); // document
--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -19,15 +19,39 @@ document.addEventListener('DOMContentLoaded', function () {
       zoom: 12,
       center:{lat: lat, lng: lng}
     };
-  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-  var marker = new google.maps.Marker({
-      position: map.getCenter(),
-      draggable: false,
-      map: map,
-      icon: "http://www.4smileys.com/smileys/food-smileys/hungry_boy.gif",
-      animation: google.maps.Animation.DROP
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    var marker = new google.maps.Marker({
+        position: map.getCenter(),
+        draggable: false,
+        map: map,
+        icon: "http://www.4smileys.com/smileys/food-smileys/hungry_boy.gif",
+        animation: google.maps.Animation.DROP
+      });
+    var geocoder = new google.maps.Geocoder();
+    var showTrucks = document.getElementById("showTrucks");
+    showTrucks.addEventListener('click', function(e){
+      e.preventDefault();
+      geocodeAddress(geocoder, map);
     });
   }
+  function geocodeAddress(geocoder, resultsMap) {
+          for (i = 0; i < objArr.length; i++) {
+            var address = JSON.stringify(objArr[i].address)
+            geocoder.geocode({'address': address}, function(results, status) {
+              if (status === google.maps.GeocoderStatus.OK) {
+                console.log(results[0].formatted_address);
+                resultsMap.setCenter(results[0].geometry.location);
+                var marker = new google.maps.Marker({
+                  map: resultsMap,
+                  position: results[0].formatted_address.geometry.location,
+                  icon: "http://howtostartafoodtruckbusinessinindia.com/wp-content/uploads/foodtruck-icon-web.png",
+                  draggable: false,
+                  map: map,
+                });
+              };
+            });
+          }; // for
+        }; // geocode
 
   getLocation();
 });

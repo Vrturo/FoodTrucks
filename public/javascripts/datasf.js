f980f011abea564fb55c7776e04d6e28f9efb7c7
Arturo Perez <VrturoPerez@gmail.com>
2015-12-30 14:48:21 -0800

    change api endpoint

--- public/javascripts/datasf.js
+++ public/javascripts/datasf.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
   var request = new XMLHttpRequest();
-  request.open("GET", "https://data.sfgov.org/resource/6a9r-agq8.json", true);
+  request.open("GET", "https://data.sfgov.org/resource/rqzj-sfat.json", true);
   request.send();
   request.onreadystatechange = function () {
     if (request.readyState != 4 || request.status != 200) return;

b4e837a3dd5b1644613a50c333649e9e43e7ab23
Arturo Perez <VrturoPerez@gmail.com>
2015-12-30 18:41:37 -0800

    clean up map.js and add notes

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -40,44 +40,10 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 
-//   function geocodeAddress(geocoder, resultsMap) {
-//     for (i = 0; i < objArr.length; i++) {
-//       var name = objArr[i].applicant;
-//       var dayshours = objArr[i].dayshours;
-//       var address = objArr[i].address;
-//       var fooditems = objArr[i].fooditems;
-//       var contentString = '<h3><strong>' + name + '</strong></h3>' +
-//                        '<strong>Address: </strong>' + address +
-//                        '<br><strong>Food</strong>: ' + fooditems +
-//                        '<br><strong>Schedule:</strong> ' + dayshours;
-//       var addressString = JSON.stringify(objArr[i].address + " San Francisco, CA");
-//       geocoder.geocode({'address': addressString}, function(results, status) {
-//         if (status === google.maps.GeocoderStatus.OK) {
-//           var marker = new google.maps.Marker({
-//             map: resultsMap,
-//             position: results[0].geometry.location,
-//             icon: "http://www.localsconnected.com/wp-content/uploads/2014/11/Food-Truck-Icon.png",
-//             draggable: false,
-//             map: map,
-//             animation: google.maps.Animation.DROP
-//           });
-//           // console.log(results);
-//           // add listener to click on marker
-//           marker.addListener('click', function() {
-//             infowindow.open(map, marker);
-//           });
-//           var infowindow = new google.maps.InfoWindow({
-//               content: contentString
-//           });
-//         }; // if
-//       }); // geocode
-//     }; // for
-//   }; // geocodeAddress
-//   getLocation();
-// }); // document
-
   function geocodeAddress(geocoder, resultsMap) {
     for (i = 0; i < objArr.length; i++) {
+      // Access i directly from the anonymous function (via closure),
+       // but it needs to be captured so that each call to geocode gets its own copy.
       (function(iter){
       var name = objArr[i].applicant;
       var dayshours = objArr[i].dayshours;
@@ -88,6 +54,8 @@ document.addEventListener('DOMContentLoaded', function () {
                        '<br><strong>Food</strong>: ' + fooditems +
                        '<br><strong>Schedule:</strong> ' + dayshours;
       var addressString = JSON.stringify(objArr[i].address + " San Francisco, CA");
+
+
         geocoder.geocode({'address': addressString}, function(results, status) {
           if (status === google.maps.GeocoderStatus.OK) {
             var marker = new google.maps.Marker({
@@ -98,7 +66,7 @@ document.addEventListener('DOMContentLoaded', function () {
               map: map,
               animation: google.maps.Animation.DROP
             });
-            console.log(name);
+
             // add listener to click on marker
             marker.addListener('click', function() {
               infowindow.open(map, marker);
@@ -108,7 +76,7 @@ document.addEventListener('DOMContentLoaded', function () {
             });
           }; // if
         }); // geocode
-      })(i);
+      })(i); // return i so it saves accesss to object
     }; // for
   }; // geocodeAddress
   getLocation();
